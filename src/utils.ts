1823609199815a3c926e4b45cf5279507ba9f881
pgolovkin <pavel@novawallet.io>
2022-07-25 10:52:28 +0300

    Fixed gradient colors support

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function getChainColor(color: string) {
+  return color.includes("linear-gradient") ? {backgroundImage: color} : { backgroundColor: color };
+}
